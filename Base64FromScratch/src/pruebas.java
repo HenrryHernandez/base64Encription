d28e1045540dda1d8927ff52a11eaf14e1202890
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-03-26 19:58:14 -0700

    Update pruebas.java

--- Base64FromScratch/src/pruebas.java
+++ Base64FromScratch/src/pruebas.java
@@ -2,6 +2,9 @@
 import java.util.List;
 import java.util.ArrayList;
 
+
+//PUEDE USAR ESTA CLASE QUE INCLUTE EL METODO MAIN PARA PODER HACER PRUEBAS DEL PROGRAMA
+
 public class pruebas {
     public static void main(String args[]) {
         Encriptar en = new Encriptar("qegffqf");
